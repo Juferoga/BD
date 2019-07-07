2fe03c221f1c92c0b05587879dccbe6cea3f53cb
Juan Felipe R <42847785+Juferoga@users.noreply.github.com>
2019-07-07 17:09:31 -0500

    Update Taller_SQL.sql

--- Taller_SQL.sql
+++ Taller_SQL.sql
@@ -20,7 +20,8 @@ SELECT c.n_nom_cliente, c.n_ape_cliente, p.n_producto, v.k_rut, vp.v_cantidad
 FROM CLIENTE c, PRODUCTO p, VENTA v, VENTA_PRODUCTO vp
 WHERE vp.k_producto = 86631 AND
 vp.k_producto = p.k_producto AND
-v.k_rut = c.k_rut
+v.k_rut = c.k_rut AND
+v.f_venta = '4/11/2019'
 
 --3) VENTAS all data que se compraron en la fecha 4/11/19
 -- Proyectar RUT,NOMBRE_PROVEEDOR,N_PRODUCTO,F_VENTA
